663812c417a1c3c83af103e56740baa393f81124
Leandro <lmacoretta@live.com.ar>
2019-05-19 10:10:07 -0300

    Inicio de dashboard y rutas privadas

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,8 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -35,6 +37,7 @@ const App = () => {
             <Switch>
               <Route path='/register' component={Register} />
               <Route path='/login' component={Login} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
