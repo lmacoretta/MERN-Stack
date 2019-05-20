073443633349d9eba325be329f37230c3dda8685
lmacoretta <leandrom@live.com.ar>
2019-05-20 14:37:47 -0300

    Inicio del formulario para crear un profile

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
 
 if (localStorage.token) {
@@ -38,6 +39,7 @@ const App = () => {
               <Route path='/register' component={Register} />
               <Route path='/login' component={Login} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              <PrivateRoute exact path='/create-profile' component={CreateProfile} />
             </Switch>
           </section>
         </Fragment>
