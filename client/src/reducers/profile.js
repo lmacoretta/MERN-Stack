efd2ec880e13706e12727bb7cbcf166eac48fbc9
lmacoretta <leandrom@live.com.ar>
2019-05-21 17:39:32 -0300

    Add experience and education form/logic

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,4 @@
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from '../actions/types';
+import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE } from '../actions/types';
 
 const initialState = {
   profile: null,
@@ -8,9 +8,10 @@ const initialState = {
   error: {}
 }
 
-export default (state = initialState, action  ) => {
+export default (state = initialState, action) => {
   switch (action.type) {
     case GET_PROFILE:
+    case UPDATE_PROFILE:
       return { ...state, profile: action.payload, loading: false }
     case PROFILE_ERROR:
       return { ...state, error: action.payload, loading: false }
