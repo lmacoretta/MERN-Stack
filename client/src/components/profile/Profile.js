c3e12a72767f28e92668f84a96d9fe196e0fa2a2
lmacoretta <leandrom@live.com.ar>
2019-06-09 22:08:08 -0300

    Add profileEducation component

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -6,6 +6,7 @@ import Spinner from '../layout/Spinner';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
 import ProfileExperience from './ProfileExperience';
+import ProfileEducation from './ProfileEducation';
 import { getProfileById } from '../../actions/profileActions';
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
@@ -40,6 +41,19 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
                     ))
                   }
                 </Fragment>) : <h4>No experience credentials</h4>
+              }
+            </div>
+
+            <div className="profile-edu bg-white p-2">
+              <h2 className="text-primary">Education</h2>
+              {
+                profile.education.length > 0 ? (<Fragment>
+                  {
+                    profile.education.map(education => (
+                      <ProfileEducation key={education._id} education={education} />
+                    ))
+                  }
+                </Fragment>) : <h4>No education credentials</h4>
               }
             </div>
           </div>
