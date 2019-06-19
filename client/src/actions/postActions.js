f09cad916f759546076e770bd2682527521ff5fd
lmacoretta <leandrom@live.com.ar>
2019-06-19 17:57:06 -0300

    Hice las actions para las likes y unlikes

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_POSTS, POST_ERROR } from './types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types';
 
-
+/** Obtengo los posts */
 export const getPosts = () => {
   return async dispatch => {
     try {
@@ -11,6 +11,44 @@ export const getPosts = () => {
       dispatch({
         type: GET_POSTS,
         payload: res.data
+      })
+    } catch (err) {
+      dispatch({
+        type: POST_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      })
+    }
+  }
+}
+
+/** Add Likes */
+export const addLike = (id) => {
+  return async dispatch => {
+    try {
+      const res = await axios.get(`api/posts/like/${id}`);
+
+      dispatch({
+        type: UPDATE_LIKES,
+        payload: { id, likes: res.data }
+      })
+    } catch (err) {
+      dispatch({
+        type: POST_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      })
+    }
+  }
+}
+
+/** Remove Likes */
+export const removeLike = (id) => {
+  return async dispatch => {
+    try {
+      const res = await axios.get(`api/posts/unlike/${id}`);
+
+      dispatch({
+        type: UPDATE_LIKES,
+        payload: { id, likes: res.data }
       })
     } catch (err) {
       dispatch({
