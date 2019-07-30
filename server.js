594d851f219ce9faa24024f5a9e7d765ef0bc003
lmacoretta <leandrom@live.com.ar>
2019-07-29 22:45:13 -0300

    Preparando todo para heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
+const path = require("path");
 
 /** Config */
 const PORT = process.env.PORT || 5000;
@@ -12,10 +13,20 @@ app.use(express.json({ extended: false }));
 connectDB();
 
 /** Routes */
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+
+/** Serve static assets in production */
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
