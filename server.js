65c9034cb4744eb18f22d4db426ea6063ad9613a
lmacoretta <leandrom@live.com.ar>
2019-07-29 22:59:10 -0300

    Correcciones menores

--- server.js
+++ server.js
@@ -3,9 +3,6 @@ const app = express();
 const connectDB = require("./config/db");
 const path = require("path");
 
-/** Config */
-const PORT = process.env.PORT || 5000;
-
 /** Middelware */
 app.use(express.json({ extended: false }));
 
@@ -27,6 +24,9 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
+
+/** Config */
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
